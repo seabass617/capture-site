6b1a9955cfdb0e281a2b154e3d010b6cd0e33067
Sebastian Viasus <vseb85@gmail.com>
2021-02-18 14:49:15 -0500

    Added Styled Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import AboutUs from "./pages/AboutUs";
+import GlobalStyle from "./components/GlobalStyle";
 
 function App() {
   return (
     <div className="app">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
