f9c438abe5b58a4dffcc311872fcf0be3b6fa90f
Sebastian Viasus <vseb85@gmail.com>
2021-02-18 14:07:22 -0500

    Adding FAQ section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How Do I Start?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam,
+            consectetur?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>How much does it cost?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam,
+            consectetur?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What products do you offer?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam,
+            consectetur?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Where can I find help?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam,
+            consectetur?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
