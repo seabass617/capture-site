c480152840956ff38265c44facf1e7f88cdfa23f
Sebastian Viasus <vseb85@gmail.com>
2021-02-19 18:31:00 -0500

    Added Router and Links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from "react";
 import AboutUs from "./pages/AboutUs";
 import GlobalStyle from "./components/GlobalStyle";
+import Nav from "./components/Nav";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+import MovieDetail from "./pages/MovieDetail";
+//Router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
       <GlobalStyle />
-      <AboutUs />
+      <Nav />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work" exact>
+          <OurWork />
+        </Route>
+        <Route path="/work/:id">
+          <MovieDetail />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
